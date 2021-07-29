cba79dfdab2517536491be59dd46997f6828dc6a
Karim <karim.alaael-din@gmx.de>
2021-07-29 22:01:48 +0100

    feat: initial grad boost for 2017 double pulse

---
+++ utility/estimators/grad_boost.py
@@ -0,0 +1,21 @@
+from deprecated import deprecated
+
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingRegressor
+import xgboost as xgb
+
+
+@deprecated("The histogram gradient boosting regressor is experimental and should not be used.")
+def fit_grad_boost(x_tr, y_tr, reg=1000, lr=0.1, iter=1000, samples=20, leaves=31):
+    reg = HistGradientBoostingRegressor(l2_regularization=reg, verbose=1, learning_rate=lr,
+                                        early_stopping=False, random_state=1, max_iter=iter,
+                                        min_samples_leaf=samples, max_leaf_nodes=leaves)
+    reg.fit(x_tr, y_tr)
+    return reg
+
+
+def fit_xgboost(x_tr, y_tr, n_est=20, n_jobs=4, gamma=0.1):
+    reg = xgb.XGBRegressor(random_state=1, n_estimators=n_est, verbosity=2, n_jobs=n_jobs, gamma=gamma)
+
+    reg.fit(x_tr, y_tr)
+    return reg
---
+++ utility/estimators/neural_network.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+
+strategy = tf.distribute.MirroredStrategy()
+
+
+# Did not use dataclass due to weird __dict__ behaviour
+class Layer:
+
+    def __init__(self, kind=Dense, units=10, activation="relu", kernel_regularizer="l2", rate=0.0):
+        self.kind = kind
+        self.units = units
+        self.activation = activation
+        self.kernel_regularizer = kernel_regularizer
+        self.rate = rate
+
+        if self.kind in (Dropout, BatchNormalization):
+            self.units = None
+            self.activation = None
+            self.kernel_regularizer = None
+            if self.kind is BatchNormalization:
+                self.rate = None
+        else:
+            self.rate = None
+
+    def get_attr(self):
+        d = self.__dict__.copy()
+        del d["kind"]
+        return {k: v for k, v in d.items() if v is not None}
+
+
+def get_layers(shape, activation, regularizer, drop_out, batch_norm):
+    layer_list = []
+    for i in shape:
+        layer_list.append(Layer(Dense, units=i, activation=activation, kernel_regularizer=regularizer))
+        layer_list.append(Layer(Dropout, rate=drop_out))
+        if batch_norm:
+            layer_list.append(Layer(BatchNormalization))
+    return layer_list
+
+
+def ann(layer_list, out_shape, loss, opt):
+    model = tf.keras.Sequential()
+    for layer in layer_list:
+        model.add(layer.kind(**layer.get_attr()))
+    model.add(Dense(units=out_shape, activation="linear"))
+    model.compile(opt, loss=loss, metrics=["mae"])
+    return model
+
+
+def fit_ann(x_tr, y_tr, layer_list, rate=0.015, loss="mae", epochs=3_000, validation_split=0.15, batch_size=1_000):
+
+    if len(y_tr.shape) > 1:
+        out_sh = y_tr.shape[1]
+    else:
+        out_sh = 1
+
+    opt = tf.keras.optimizers.SGD(learning_rate=rate)
+    est = ann(layer_list, out_sh, loss, opt)
+    hist = est.fit(x_tr, y_tr, batch_size,
+                   epochs=epochs, verbose=2,
+                   validation_split=validation_split)
+    return est, hist
