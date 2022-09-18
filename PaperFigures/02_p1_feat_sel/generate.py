0750926d3cfef7f5af55d67a4de8f10317ffcc64
KarimAED <karim.alaael-din@gmx.de>
2022-09-18 13:15:03 +0100

    feature selection plots

--- PaperFigures/02_p1_feat_sel/generate.py
+++ PaperFigures/02_p1_feat_sel/generate.py
@@ -11,6 +11,9 @@ STR_DATA = {
 }
 
 FEAT_RANK = feature.feature_ranking_pipeline(DATA, STR_DATA, ann_pipeline)
+FEAT_RANK.to_csv("PaperFigures/02_p1_feat_sel/feat_sel_data.csv")
+
+
 FEAT_RANK_AND_SELECTED = feature.selected_refit_pipeline(
     DATA,
     STR_DATA,
--- PaperFigures/s1_delay_feat_sel/generate.py
+++ PaperFigures/s1_delay_feat_sel/generate.py
@@ -9,6 +9,8 @@ STR_DATA = {
 }
 
 FEAT_RANK = feature.feature_ranking_pipeline(DATA, STR_DATA, ann_pipeline)
+
+
 FEAT_RANK_AND_SELECTED = feature.selected_refit_pipeline(
     DATA,
     STR_DATA,
